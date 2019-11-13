08d936a530b74a0ee34f02971f66434a9a01faa4
Dmitriy Kovalyov <dmitriy.kovalyov@ddgcorp.ru>
2019-11-13 15:43:21 +0300

    Fixes after rebasing

--- packages/bitcore-wallet/bin/cli-utils.js
+++ packages/bitcore-wallet/bin/cli-utils.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 var url = require('url');
 var read = require('read')
 var log = require('npmlog');
-var Client = require('../../ducatuscore-wallet-client');
+var Client = require('../../ducatus-wallet-client');
 var FileStorage = require('./filestorage');
 var sjcl = require('sjcl');
 
